d76cdd0ac535e110f8729394fbfd5a7912a9c9f5
Felipe Uenes <uenesdesigner@gmail.com>
2023-04-05 13:19:12 -0300

    ajuste

--- src/pages/Local/index.jsx
+++ src/pages/Local/index.jsx
@@ -7,7 +7,7 @@ export function Local(){
 
             <section>
                 <h1>Onde estamos?</h1>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3045.2589393555513!2d-39.52472158609443!3d-6.521923895282528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7a2f71cca748c23%3A0x7e5168671208db6!2sPolo%20Artesanal%20de%20Juc%C3%A1s!5e1!3m2!1spt-BR!2sbr!4v1680702936331!5m2!1spt-BR!2sbr" width="600" height="450" ></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3045.2589393555513!2d-39.52472158609443!3d-6.521923895282528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7a2f71cca748c23%3A0x7e5168671208db6!2sPolo%20Artesanal%20de%20Juc%C3%A1s!5e1!3m2!1spt-BR!2sbr!4v1680702936331!5m2!1spt-BR!2sbr" width="500" height="400" ></iframe>
                 <h2>Polo Artesanal, Jucás/CE</h2>
             </section>
         </div>
