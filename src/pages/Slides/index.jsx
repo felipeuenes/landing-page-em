12db200d73ff2cb3a689812876bd80d850e5a90e
Felipe Uenes <uenesdesigner@gmail.com>
2023-04-05 15:26:37 -0300

    add

--- src/pages/Slides/index.jsx
+++ src/pages/Slides/index.jsx
@@ -4,6 +4,8 @@ import image from '../../images/image.jpg'
 import image2 from '../../images/image2.jpg'
 import image3 from '../../images/image3.jpg'
 import image4 from '../../images/image4.jpg'
+import image5 from '../../images/image5.jpg'
+import image6 from '../../images/image6.jpg'
 
 
 
@@ -17,6 +19,8 @@ export function Slider(){
           <div className="embla__slide"><img src={image2} alt="" /></div>
           <div className="embla__slide"><img src={image3} alt="" /></div>
           <div className="embla__slide"><img src={image4} alt="" /></div>
+          <div className="embla__slide"><img src={image5} alt="" /></div>
+          <div className="embla__slide"><img src={image6} alt="" /></div>
         </div>
       </div>
     )
